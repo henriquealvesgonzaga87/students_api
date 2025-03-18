4accd8d0f3cb69eaacc964bc7fd9df7a17dd29e6
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2025-03-18 14:43:35 +0000

    fixes import user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import UserController from '../controllers/userController';
+import UserController from '../controllers/UserController';
 
 import loginRequired from '../middlewares/loginRequired'
 
