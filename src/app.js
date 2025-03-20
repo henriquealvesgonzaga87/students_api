888994dddf102600cccdf662238f6d0213d5f2f0
henriquealvesgonzaga87 <henriquealves87@hotmail.com>
2025-03-20 20:56:32 +0000

    add cors

--- src/app.js
+++ src/app.js
@@ -5,12 +5,31 @@ dotenv.config();
 import './database';
 
 import express from 'express'
+import cors from 'cors';
+import helmet from 'helmet';
 
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes'
 import tokenRoutes from './routes/tokenRoutes'
 import studentRoutes from './routes/studentRoutes'
 import photoRoutes from './routes/photoRoutes'
+
+
+const whiteList = [
+  "https://www.studentsbackend.hagpythondev.com/",
+  "http://localhost:3000"
+];
+
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if(whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
 
 
 class App {
@@ -21,6 +40,8 @@ class App {
   }
 
   middlewares(){
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({extended: true}));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
